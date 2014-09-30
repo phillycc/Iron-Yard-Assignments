95331822e4beb88e3edd8046c08e75d477bb4630
David Elmer <davelmer@hotmail.com>
2014-09-30 17:56:50 -0400

    @asoper29 @phillycc

--- dojo--2014-09-30.js
+++ dojo--2014-09-30.js
@@ -1,1 +1,10 @@
-console.log("Hello World2!");
+var multiply = function(a,b){
+  if (b === "one") {
+    return 1;
+  }
+  return 0;
+}
+console.log('it should multiply "one" and "zero: ',
+  multiply("one","zero") === 0);
+console.log('it should multiply "one" and "one: ',
+  multiply("one","one") === 1);

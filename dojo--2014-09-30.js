8bc5ce8054c337c760534b49688c0f6a33436fe6
David Elmer <davelmer@hotmail.com>
2014-09-30 18:02:30 -0400

    @asoper29 @phillycc

--- dojo--2014-09-30.js
+++ dojo--2014-09-30.js
@@ -4,7 +4,21 @@ var multiply = function(a,b){
   }
   return 0;
 }
-console.log('it should multiply "one" and "zero: ',
+console.log('it should multiply "one" and "zero": ',
   multiply("one","zero") === 0);
-console.log('it should multiply "one" and "one: ',
+console.log('it should multiply "one" and "one": ',
   multiply("one","one") === 1);
+console.log('it should multiply "one" and "two": ',
+  multiply("one", "two") === 2);
+console.log('it should multiply "one" and "zero": ',
+  multiply("two","zero") === 0);
+console.log('it should multiply "one" and "one": ',
+  multiply("two","one") === 2);
+console.log('it should multiply "one" and "two": ',
+  multiply("two", "two") === 4);
+console.log('it should multiply "one" and "zero": ',
+  multiply("three","zero") === 0);
+console.log('it should multiply "one" and "one": ',
+  multiply("three","one") === 1);
+console.log('it should multiply "one" and "two": ',
+  multiply("three", "two") === 6);

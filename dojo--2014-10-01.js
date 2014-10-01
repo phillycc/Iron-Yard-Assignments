d3b9d4c521920802c7349128121f6916d4622ff1
David Elmer <davelmer@hotmail.com>
2014-10-01 14:46:33 -0400

    @phillycc @kimkvn

--- dojo--2014-10-01.js
+++ dojo--2014-10-01.js
@@ -1,9 +1,21 @@
 
-function convertToWords(){
-  return "one";
+function convertToWords(a){
+  if(a === 1){
+    return "one";
+  }
+  if(a === 2){
+    return "two";
+  }
+  if(a === 3){
+    return "three";
+  }
 }
 
 console.log('if it is 1: ',
   convertToWords(1) === "one");
 console.log('if it is 2: ',
   convertToWords(2) === "two");
+console.log('if it is 3: ',
+  convertToWords(3) === "three");
+console.log('if it is 11: ',
+  convertToWords(11) === "eleven");

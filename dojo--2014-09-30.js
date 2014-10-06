994e074646b4ec9ff6cfe855c2ee790b65d653dd
David Elmer <davelmer@hotmail.com>
2014-10-06 19:44:39 -0400

    Commented out nested for-loops

--- dojo--2014-09-30.js
+++ dojo--2014-09-30.js
@@ -26,16 +26,6 @@ console.log('it should multiply "' + a + '" and "' + b + '": ',
   multiply(a, b) === c);
 }
 
-var iCount=0;
-letterNum.forEach(function(i){
-    var jCount=0;
-    letterNum.forEach(function(j){
-      print(i, j, iCount*jCount);
-      jCount++;
-  });
-  iCount++;
-});
-
 //for (var j = 1; j <= 10; j++){
 //  for (var i = 0; i < letterNum.length; i++){
 //    print(letterNum[j], letterNum[i], i*j);

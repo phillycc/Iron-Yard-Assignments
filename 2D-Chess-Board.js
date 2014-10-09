a8d8bcd2d17a9e9f2b0235babeb21531a31d4174
David Elmer <davelmer@hotmail.com>
2014-10-09 16:02:00 -0400

    Next 3 moves

--- 2D-Chess-Board.js
+++ 2D-Chess-Board.js
@@ -20,9 +20,29 @@ board[6][3] = ' ';
 // Move Black King's Pawn forward 2
 board[3][3] = board[1][3];
 board[1][3] = ' ';
-console.log(board.join('\n') + '\n\n');
+//console.log(board.join('\n') + '\n\n');
 
-// Move White Knight forward 2, left 1
+// Move White Right Knight forward 2, left 1
 board[5][5] = board[7][6];
 board[7][6] = ' ';
+//console.log(board.join('\n') + '\n\n');
+
+// Move Black Left Knight forward 2, right 1
+board[2][5] = board[0][6];
+board[0][6] = ' ';
+//console.log(board.join('\n') + '\n\n');
+
+// Move White Right Knight's Pawn forward 1
+board[5][6] = board[6][6];
+board[6][6] = ' ';
+//console.log(board.join('\n') + '\n\n');
+
+// Move Black King's Pawn forward 1
+board[2][4] = board[1][4];
+board[1][4] = ' ';
+console.log(board.join('\n') + '\n\n');
+
+// Move White Bishop forward right 1
+board[6][6] = board[7][5];
+board[7][5] = ' ';
 console.log(board.join('\n') + '\n\n');

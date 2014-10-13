0ef23b9102fc878af1c90528a89ab4a157893e2e
David Elmer <davelmer@hotmail.com>
2014-10-13 16:30:12 -0400

    Updated string calc

--- dojo--2014-10-08.js
+++ dojo--2014-10-08.js
@@ -1,23 +1,20 @@
+
+
 var assert = require('assert');
 
 var words = { zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9, ten: 10 };
 
-
-function fromEnglish(word){
-   return words[word];
-}
-
-var multiply = function(a,b){
-  a = fromEnglish(a);
-  b = fromEnglish(b);
-  return a * b;
+var multiply = {
+  wordsToNum : function(wordA, wordB){
+    return words[wordA] * words[wordB];
+  }
 }
 
 suite('mulitply()', function(){
     for (var word1 in words){
        for (var word2 in words){
            test('should multiply ' +word1+ ' and '+word2, function(){
-               assert.strictEqual(multiply(word1,word2),words[word1]*words[word2]);
+               assert.strictEqual(multiply.wordsToNum(word1,word2),words[word1]*words[word2]);
            });
        }
     }

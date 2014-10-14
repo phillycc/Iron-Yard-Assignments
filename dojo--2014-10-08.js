08815b4f89e4bb6d2567a776b54770a839e09713
David Elmer <davelmer@hotmail.com>
2014-10-13 20:58:02 -0400

    Objectified string calculator

--- dojo--2014-10-08.js
+++ dojo--2014-10-08.js
@@ -1,39 +1,47 @@
 
-
 var assert = require('assert');
 
-var words = { zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9, ten: 10 };
+function stringCalc(){
+  var words = { zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9, ten: 10 };
 
-var multiply = {
-  wordsToNum : function(wordA, wordB){
-    return words[wordA] * words[wordB];
+  return {
+    fromEnglish : function(wordA, wordB){
+      return words[wordA] * words[wordB];
+    },
   }
 }
 
-suite('mulitply()', function(){
-    for (var word1 in words){
-       for (var word2 in words){
+/*
+suite('stringCalc()', function(){
+    for (var word1 in stringCalc().words){
+       for (var word2 in stringCalc().words){
            test('should multiply ' +word1+ ' and '+word2, function(){
-               assert.strictEqual(multiply.wordsToNum(word1,word2),words[word1]*words[word2]);
+               assert.strictEqual(stringCalc().fromEnglish(word1,word2),words[word1]*words[word2]);
            });
        }
     }
-});
+});*/
 
-/*suite('mulitply()', function(){
+suite('stringCalc()', function(){
+    test('should multiply "zero" and "zero"', function(){
+        assert.equal(stringCalc().fromEnglish('zero','zero'),0);
+    });
     test('should multiply "one" and "one"', function(){
-        assert.equal(multiply('one','one'),1);
+        assert.equal(stringCalc().fromEnglish('one','one'),1);
     });
     test('should multiply "one" and "two"', function(){
-        assert.equal(multiply('one','two'),2);
+        assert.equal(stringCalc().fromEnglish('one','two'),2);
     });
     test('should multiply "one" and "three"', function(){
-        assert.equal(multiply('one','three'),3);
+        assert.equal(stringCalc().fromEnglish('one','three'),3);
     });
     test('should multiply "one" and "four"', function(){
-        assert.equal(multiply('one','four'),4);
+        assert.equal(stringCalc().fromEnglish('one','four'),4);
     });
-    test('should multiply "zero" and "zero"', function(){
-        assert.equal(multiply('zero','zero'),0);
+    test('should multiply "nine" and "nine"', function(){
+        assert.equal(stringCalc().fromEnglish('nine','nine'),81);
     });
-});*/
+    test('should multiply "ten" and "ten"', function(){
+        assert.equal(stringCalc().fromEnglish('ten','ten'),100);
+    });
+});

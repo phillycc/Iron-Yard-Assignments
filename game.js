ff4d25a68211e03bb07fde4e26ea847d69c1a979
David Elmer <davelmer@hotmail.com>
2014-10-15 03:07:36 -0400

    @philly @loganarnett @asoper29 @mema82

--- game.js
+++ game.js
@@ -11,8 +11,9 @@ function board(){
 function Game(){
     this.board = board();
 }
-
 /**
+ * Checks cell at {x,y} for "aliveness"
+ *
  * @param Number x coordinate
  * @param Number y coordinate
  * @return Boolean if cell at {x,y} is alive
@@ -34,6 +35,7 @@ Game.prototype.setAlive = function(x,y){
 
 /**
  * Make the cell at {x,y} "dead"
+ *
  * @param Number x coordinate
  * @param Number y coordinate
  * @return undefined
@@ -41,14 +43,85 @@ Game.prototype.setAlive = function(x,y){
 Game.prototype.setDead = function(x, y){
     this.board[x][y] = false;
 }
-
 /**
- * Update the `board` by applying the `rules`
+ * Returns list of all neighboring cells
+ * of cell at {x,y} inside a 3x3 board
+ *
+ * @param Number x coordinate
+ * @param Number y coordinate
+ * @return Array of values from `board()` that are neighbors of cell at {x,y}
  */
-Game.prototype.rules = function(){
+Game.prototype.neighborsOf = function(x,y){
+    var diffs = [ -1, 0, +1 ],
+    neighbors = [ ];
+    board = this.board;
 
+    diffs.forEach(function(dX){
+        diffs.forEach(function(dY){
+            if (board.length === 0) return;
+            // skip this cell
+            if ( dX === 0 && dY === 0 ) return;
+            //skip if row index does not exist
+            if (((x + dX) < 0) || ((x + dX)>2)) return;
+            //skip if col index does not exist
+            if (((y + dY) < 0) || ((y + dY)>2)) return;
+            neighbors.push(board[x + dX][y + dY]);
+        });
+    });
+    return neighbors;
+}
+/**
+ * Returns state of a cell as alive or dead based
+ * on the rules of Conway's Game of Life
+ *
+ * @param Boolean cell
+ * @param Array neighbors
+ * @return Boolean state of cell
+ */
+Game.prototype.rules = function(cell, neighbors){
+      var liveNeighbors = neighbors.filter(function(value){
+        return value;
+      });
+      liveNeighbors = liveNeighbors.length;
+      //apply rules
+      if (cell && liveNeighbors===2) return true;
+      if (liveNeighbors===3) return true;
+      return false;
+}
+/**
+ *
+ * The tick function is passed a 3x3 board,
+ * applies Conway's Game of Life rules, and
+ * returns a 3x3 board after a single tick.
+ *
+ * Function Definition: tick()
+ * @return: Array of Array of Boolean
+ *
+**/
+Game.prototype.tick = function(){
+    var nextBoard = board();
+    for (var x = 0; x < this.board.length; x++){
+        for(var y = 0; y < this.board[x].length; y++){
+           nextBoard[x][y]=this.rules(this.board[x][y], this.neighborsOf(x,y));
+        };
+    };
+    this.board = nextBoard;
+    return this.board;
 }
 
 Game.prototype.display = function(){
+    var spacer = '+---+---+---+\n';
 
-}
+    return spacer +
+        // Apply `renderRow` to each `row` in `board`...
+        this.board.map(function renderRow(row){
+            return '| ' +
+                // Apply `renderCell` to each `cell` in `row`...
+                row.map(function renderCell(cell){
+                    // return 'X' if `cell` is TRUTHY otherwise return ' '
+                    return cell && 'X' || ' ';
+                }).join(' | ') // Place ' | ' between each `cell`...
+            + ' |\n';
+        }).join(spacer) // Place `spacer` between each `row`...
+        + spacer;
+} // END display
--- gameTest.js
+++ gameTest.js
@@ -4,46 +4,74 @@ var should = require('chai').should();
 
 var Game = require('./game.js');
 
-  describe('Conway\'s Game of Life: Voodoo Black Magic Edition\n', function(){
-      var aGame = new Game();
+  describe('Conway LITE: Voodoo Black Magic Edition\n', function(){
+      var game = new Game();
+
+      /*describe('Constructor function tick()', function(){
+          it('should be a type of function\n', function(){
+              assert.isFunction(game.tick);
+          });
+
+          /*describe('WHEN the board has fewer than 3 adjacent cells', function(){
+              it('should die in 1 tick', function(){
+                  //game.setAlive(0,0);
+                  assert.deepEqual(game.tick(), game.board);
+              });
+          });
+          describe('WHEN the board has 3 adjacent cells in a line', function(){
+              game.setAlive(0,1);
+              game.setAlive(1,1);
+              game.setAlive(2,1);
+              before(function(){
+                  expected = [
+                      [ false, false, false ],
+                      [ true,  true,  true  ],
+                      [ false, false, false ],
+                  ];
+              });
+              it('should oscilate every tick', function(){
+                  assert.deepEqual(game.tick(), expected);
+              });
+          });
+      });
 
       describe('Constructor function rules()', function(){
           it('should be a type of function\n', function(){
-              assert.isFunction(aGame.rules);
+              assert.isFunction(game.rules);
           });
           describe('GENERATION RULE', function(){
               it('should NOT generate new cell if fewer than 3 neighbors', function(){
-                  expect(aGame.rules(false, [ ]), false).to.be.false;
-                  expect(aGame.rules(false, [ true ]), false).to.be.false;
-                  expect(aGame.rules(false, [ true, true ]), false).to.be.false;
-                  expect(aGame.rules(false, [ false, false, false ]), false).to.be.false;
+                  expect(game.rules(false, [ ]), false).to.be.false;
+                  expect(game.rules(false, [ true ]), false).to.be.false;
+                  expect(game.rules(false, [ true, true ]), false).to.be.false;
+                  expect(game.rules(false, [ false, false, false ]), false).to.be.false;
               });
               it('should generate new cell if exactly 3 neighbors', function(){
-                  expect(aGame.rules(false, [ true, true, true ]), true).to.be.true;
+                  expect(game.rules(false, [ true, true, true ]), true).to.be.true;
               });
               it('should NOT generate new cell if MORE than 3 neighbors', function(){
-                  expect(aGame.rules(false, [ true, true, true, true ]), false).to.be.false;
-                  expect(aGame.rules(false, [ true, true, true, true, true ]), false).to.be.false;
+                  expect(game.rules(false, [ true, true, true, true ]), false).to.be.false;
+                  expect(game.rules(false, [ true, true, true, true, true ]), false).to.be.false;
               });
           });
           describe('UNDER-POPULATION RULE', function(){
               it('should die if there are fewer than 2 or 3 neighbors', function(){
-                  expect(aGame.rules(true, [ ]), false).to.be.false;
-                  expect(aGame.rules(true, [ true ]), false).to.be.false;
-                  expect(aGame.rules(true, [ false, false ]), false).to.be.false;
-                  expect(aGame.rules(true, [ false, false, false ]), false).to.be.false;
+                  expect(game.rules(true, [ ]), false).to.be.false;
+                  expect(game.rules(true, [ true ]), false).to.be.false;
+                  expect(game.rules(true, [ false, false ]), false).to.be.false;
+                  expect(game.rules(true, [ false, false, false ]), false).to.be.false;
               });
           });
           describe('SURVIVAL RULE', function(){
               it('should survive if there are exactly 2 or 3 neighbors', function(){
-                  expect(aGame.rules(true, [ true, true ]), true).to.be.true;
-                  expect(aGame.rules(true, [ true, true, true ]), true).to.be.true;
+                  expect(game.rules(true, [ true, true ]), true).to.be.true;
+                  expect(game.rules(true, [ true, true, true ]), true).to.be.true;
               });
           });
           describe('OVER-POPULATION RULE', function(){
               it('should die if there are more than 3 neighbors\n', function(){
-                  expect(aGame.rules(true, [ true, true, true, true ]), false).to.be.false;
-                  expect(aGame.rules(true, [ true, true, true, true, true ]), false).to.be.false;
+                  expect(game.rules(true, [ true, true, true, true ]), false).to.be.false;
+                  expect(game.rules(true, [ true, true, true, true, true ]), false).to.be.false;
               });
           });
       });
@@ -51,48 +79,48 @@ var Game = require('./game.js');
 
       describe('Constructor function neighborsOf()', function(){
         it('should be a type of function\n', function(){
-            assert.isFunction(aGame.neighborsOf);
+            assert.isFunction(game.neighborsOf);
         });
         describe('GIVEN a 3x3 board', function(){
             it('should return three neighbors for the corners', function(){
                 assert.deepEqual(
-                    aGame.neighborsOf(0,0),
+                    game.neighborsOf(0,0),
                     [ false, false, false ]
                 );
                 assert.deepEqual(
-                    aGame.neighborsOf(0,2),
+                    game.neighborsOf(0,2),
                     [ false, false, false ]
                 );
                 assert.deepEqual(
-                    aGame.neighborsOf(2,0),
+                    game.neighborsOf(2,0),
                     [ false, false, false ]
                 );
                 assert.deepEqual(
-                    aGame.neighborsOf(2,2),
+                    game.neighborsOf(2,2),
                     [ false, false, false ]
                 );
             });
             it('should return 5 cells for the edges', function(){
                 assert.deepEqual(
-                    aGame.neighborsOf(0,1),
+                    game.neighborsOf(0,1),
                     [ false, false, false, false, false ]
                 );
                 assert.deepEqual(
-                    aGame.neighborsOf(1,0),
+                    game.neighborsOf(1,0),
                     [ false, false, false, false, false ]
                 );
                 assert.deepEqual(
-                    aGame.neighborsOf(1,2),
+                    game.neighborsOf(1,2),
                     [ false, false, false, false, false ]
                 );
                 assert.deepEqual(
-                    aGame.neighborsOf(2,1),
+                    game.neighborsOf(2,1),
                     [ false, false, false, false, false ]
                 );
             });
             it('should return all cells for the middle\n', function(){
                 assert.deepEqual(
-                    aGame.neighborsOf(1,1),
+                    game.neighborsOf(1,1),
                     [ false, false, false, false, false, false, false, false ]
                 );
             });
@@ -101,45 +129,104 @@ var Game = require('./game.js');
 
     describe('Constructor function setAlive()', function(){
         it('should be a function', function(){
-            assert.isFunction(aGame.setAlive);
+            assert.isFunction(game.setAlive);
         });
         it('should return undefined', function(){
-            assert.isUndefined(aGame.setAlive(0,0));
+            assert.isUndefined(game.setAlive(0,0));
         });
         it('should set false cell to true\n', function(){
-            assert.isTrue(aGame.isAlive(0,0));
+            assert.isTrue(game.isAlive(0,0));
         });
     });
 
     describe('Constructor function setDead()', function(){
         it('should be a function', function(){
-            assert.isFunction(aGame.setDead);
+            assert.isFunction(game.setDead);
         });
         it('should return undefined', function(){
-            assert.isUndefined(aGame.setDead(0,0));
+            assert.isUndefined(game.setDead(0,0));
         });
         it('should set false cell to false\n', function(){
-            assert.isFalse(aGame.isAlive(0,0));
+            assert.isFalse(game.isAlive(0,0));
         });
     });
 
     describe('Constructor function isAlive()', function(){
         it('should be a function', function(){
-            assert.isFunction(aGame.isAlive);
+            assert.isFunction(game.isAlive);
         });
         it('should return a Boolean', function(){
-            assert.isBoolean(aGame.isAlive(0,0));
+            assert.isBoolean(game.isAlive(0,0));
         });
         it('should return false if cell state is false\n', function(){
-            assert.isFalse(aGame.isAlive(0,0));
-            assert.isFalse(aGame.isAlive(0,1));
-            assert.isFalse(aGame.isAlive(0,2));
-            assert.isFalse(aGame.isAlive(1,0));
-            assert.isFalse(aGame.isAlive(1,1));
-            assert.isFalse(aGame.isAlive(1,2));
-            assert.isFalse(aGame.isAlive(2,0));
-            assert.isFalse(aGame.isAlive(2,1));
-            assert.isFalse(aGame.isAlive(2,2));
+            assert.isFalse(game.isAlive(0,0));
+            assert.isFalse(game.isAlive(0,1));
+            assert.isFalse(game.isAlive(0,2));
+            assert.isFalse(game.isAlive(1,0));
+            assert.isFalse(game.isAlive(1,1));
+            assert.isFalse(game.isAlive(1,2));
+            assert.isFalse(game.isAlive(2,0));
+            assert.isFalse(game.isAlive(2,1));
+            assert.isFalse(game.isAlive(2,2));
+        });
+    });*/
+    describe('Conway LITE API', function(){
+        it('should play the game', function(){
+            assert.deepEqual(game.board, [
+              [ false, false, false ],
+              [ false, false, false ],
+              [ false, false, false ],
+            ]);
+
+            game.setAlive(0,1);
+            game.setAlive(1,1);
+            game.setAlive(2,1);
+
+            assert.isTrue(game.isAlive(0,1));
+            assert.isTrue(game.isAlive(1,1));
+            assert.isTrue(game.isAlive(2,1));
+
+            assert.equal(game.display(),
+              "+---+---+---+\n" +
+              "|   | X |   |\n" +
+              "+---+---+---+\n" +
+              "|   | X |   |\n" +
+              "+---+---+---+\n" +
+              "|   | X |   |\n" +
+              "+---+---+---+\n"
+            );
+
+            game.tick();
+
+            assert.isTrue(game.isAlive(1,0));
+            assert.isTrue(game.isAlive(1,1));
+            assert.isTrue(game.isAlive(1,2));
+
+            assert.equal(game.display(),
+              "+---+---+---+\n" +
+              "|   |   |   |\n" +
+              "+---+---+---+\n" +
+              "| X | X | X |\n" +
+              "+---+---+---+\n" +
+              "|   |   |   |\n" +
+              "+---+---+---+\n"
+            );
+            /*
+            game.tick();
+
+            game.isTrue(game.isAlive(0,1));
+            game.isTrue(game.isAlive(1,1));
+            game.isTrue(game.isAlive(2,1));
+
+            assert.equal(game.display(),
+              "+---+---+---+\n" +
+              "|   | X |   |\n" +
+              "+---+---+---+\n" +
+              "|   | X |   |\n" +
+              "+---+---+---+\n" +
+              "|   | X |   |\n" +
+              "+---+---+---+\n"
+            );*/
         });
     });
 });

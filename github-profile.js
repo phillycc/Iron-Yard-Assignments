14a72f32170f8f49999162bad5ca7f3b088d4698
David Elmer <davelmer@hotmail.com>
2014-11-09 08:21:32 -0500

    Added repo details JSON

--- github-profile.js
+++ github-profile.js
@@ -1,3 +1,108 @@
+//Call ajax function on page load
+$(function() {
+    $.ajax({
+  			"url":"https://api.github.com/users/phillycc",
+  			"dataType":"json",
+  			"success":function(data){
+          $('#avatar').attr('href',data.avatar_url).children('img').attr('src', data.avatar_url);
+  			  $('.sidebar > h3').children('span').text(data.name).siblings('small').text(data.login);
+          $('#location').text(data.location).siblings('#joined').text(data.created_at);
+          $('#email').children('a').attr('href','mailto:'+data.email).text(data.email);
+          $('#followers').children('strong').text(data.followers);
+          $('#following').children('strong').text(data.following);
+        },
+  			"error":function(data){
+  				console.log("error");
+  			}
+		});
+    $.ajax({
+      "url":"https://api.github.com/users/phillycc/starred",
+      "dataType":"json",
+      "success":function(data){
+        $('#starred').children('strong').text(data.length);
+      },
+      "error":function(data){
+        console.log("error");
+      }
+    });
+    $.ajax({
+      "url":"https://api.github.com/users/phillycc/repos",
+      "dataType":"json",
+      "success":function(data){
+        var icon;
+        var star = "<span class='octicon octicon-star'>"
+        $.each (data, function (value) {
+            if(data[value].fork){
+               icon = "<span class='octicon octicon-repo-forked'></span>";
+            } else {
+               icon = "<span class='octicon octicon-repo'></span>";
+            }
+            $('#repoItems').append('<li class="repoItem list-group-item"><a href="'+data[value].html_url+'"><div class="repoIcon">'+icon+'</div><div class="repoInfo"><strong>'+data[value].name+'</strong><br/>'+data[value].description.slice(0,37)+'</div> <div class="repoStars">0 '+star+'</div><br style="clear: left;" /></a></li>');
+        });
+      },
+      "error":function(data){
+        console.log("error");
+      }
+    });
+
+    $.ajax({
+      "url":"https://api.github.com/search/code?q=repo:rgcoders/Surf-Paddle",
+      "dataType":"json",
+      "success":function(data){
+        console.log("sucess");
+
+        /*var icon;
+        var star = "<span class='octicon octicon-star'>"
+        $.each (data, function (value) {
+            if(data[value].fork){
+               icon = "<span class='octicon octicon-repo-forked'></span>";
+            } else {
+               icon = "<span class='octicon octicon-repo'></span>";
+            }
+            $('#repoItems').append('<li class="repoItem list-group-item"><a href="'+data[value].html_url+'"><div class="repoIcon">'+icon+'</div><div class="repoInfo"><strong>'+data[value].name+'</strong><br/>'+data[value].description.slice(0,37)+'</div> <div class="repoStars">0 '+star+'</div><br style="clear: left;" /></a></li>');
+        });*/
+      },
+      "error":function(data){
+        console.log("error");
+      }
+    });
+
+
+
+
+
+});
+
+/*
+
+<li class="list-group-item"><strong></strong></li>
+<li class="list-group-item">Dapibus ac facilisis in</li>
+<li class="list-group-item">Morbi leo risus</li>
+
+$.each(response, function(i, item) {
+    $('<tr>').html("<td>" + response[i].rank + "</td><td>" + response[i].content + "</td><td>" + response[i].UID + "</td>").appendTo('#records_table');
+});
+
+    "description": "The Handbook for the Front End Engineering class
+
+  $.each(data, function(i, item){
+              alert("Mine is " + i + "|" + item.title + "|" + item.key);
+          });
+
+    */
+
+
+
+/*jQuery(function(){
+  jQuery.ajax({
+			"url":"https://api.github.com/users/phillycc",
+      "dataType":"json",
+			"success":function(data){
+
+      }
+});*/
+
+
 /*var profile = [];
 $(function(){
   jQuery.ajax('octocat.json',{

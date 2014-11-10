b5fd6d258546ef2b399fb1177275296c56a57ab6
David Elmer <davelmer@hotmail.com>
2014-11-10 07:23:45 -0500

    Completed navbar and sidebar styling

--- github-profile.js
+++ github-profile.js
@@ -6,7 +6,8 @@ $(function() {
   			"success":function(data){
           $('#avatar').attr('href',data.avatar_url).children('img').attr('src', data.avatar_url);
   			  $('.sidebar > h3').children('span').text(data.name).siblings('small').text(data.login);
-          $('#location').text(data.location).siblings('#joined').text(data.created_at);
+          $('#location').children('small').text(data.location);
+          $('#joined').children('small').text('Joined on '+new Date(data.created_at).toDateString().slice(3));
           $('#email').children('a').attr('href','mailto:'+data.email).text(data.email);
           $('#followers').children('strong').text(data.followers);
           $('#following').children('strong').text(data.following);
@@ -38,39 +39,14 @@ $(function() {
                icon = "<span class='octicon octicon-repo'></span>";
             }
             $('#repoItems').append('<li class="repoItem list-group-item"><a href="'+data[value].html_url+'"><div class="repoIcon">'+icon+'</div><div class="repoInfo"><strong>'+data[value].name+'</strong><br/>'+data[value].description.slice(0,37)+'</div> <div class="repoStars">0 '+star+'</div><br style="clear: left;" /></a></li>');
+            $('#panelRepoItems').append('<li class="list-group-item">'+data[value].name+'</li>');
+
         });
       },
       "error":function(data){
         console.log("error");
       }
     });
-
-    $.ajax({
-      "url":"https://api.github.com/search/code?q=repo:rgcoders/Surf-Paddle",
-      "dataType":"json",
-      "success":function(data){
-        console.log("sucess");
-
-        /*var icon;
-        var star = "<span class='octicon octicon-star'>"
-        $.each (data, function (value) {
-            if(data[value].fork){
-               icon = "<span class='octicon octicon-repo-forked'></span>";
-            } else {
-               icon = "<span class='octicon octicon-repo'></span>";
-            }
-            $('#repoItems').append('<li class="repoItem list-group-item"><a href="'+data[value].html_url+'"><div class="repoIcon">'+icon+'</div><div class="repoInfo"><strong>'+data[value].name+'</strong><br/>'+data[value].description.slice(0,37)+'</div> <div class="repoStars">0 '+star+'</div><br style="clear: left;" /></a></li>');
-        });*/
-      },
-      "error":function(data){
-        console.log("error");
-      }
-    });
-
-
-
-
-
 });
 
 /*

afcca11128e70f3dfa67d3f758a7672198d29595
David Elmer <davelmer@hotmail.com>
2014-11-12 02:06:04 -0500

    CSS and testing JS

---
+++ image-slider.js
@@ -0,0 +1,15 @@
+
+//var currenSlide = 1;
+var $slider = $('#slider');
+var $slideContainer = $slider.find('.slides');
+var $slides = $slideContainer.find('slide');
+
+
+
+//var firstImage = $slides.find('a').index();
+console.log($slideContainer.length);
+//var lastImage = $slides.find('img:last').index();
+//console.log(lastImage);
+
+// currentSlide++;
+//if (currentSlide === $slides.length) { currentSlide = 1;}
